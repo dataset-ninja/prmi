4b4e5fcd1c167f2a4bb7952e251781bc5cb2efbb
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-10-18 23:11:13 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -20,7 +20,7 @@ HIDE_DATASET = False  # set False when 100% sure about repo quality
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = License.CC0_1_0()
+LICENSE: License = License.CC0_1_0(source_url="https://zenodo.org/records/5974905#.YlAJtn9Bzmg")
 APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Agricultural()]
 CATEGORY: Category = Category.Agriculture(extra=Category.Biology())
 
